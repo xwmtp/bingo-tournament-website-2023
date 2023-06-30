4be4187ddb72bc3df9d8eca0184d1cabc1d81e7a
xwmtp <xwillmtp@gmail.com>
2023-06-30 12:30:46 +0200

    Add 2022 to previous years

--- src/components/pages/about/PreviousYears.tsx
+++ src/components/pages/about/PreviousYears.tsx
@@ -4,25 +4,34 @@ import styled from "styled-components";
 
 export const PreviousYears: React.FC = () => {
   return (
-    <>
-      <ImageBlockBottomMargin
+    <Container>
+      <ImageBlock
+        text={"2022"}
+        url={"https://xwmtp.github.io/bingo2022/"}
+        image={
+          "https://raw.githubusercontent.com/xwmtp/bingo-tournament-website-2023/assets/images/previousYears/2022/background_480p.png"
+        }
+      />
+      <ImageBlock
         text={"2021"}
         url={"https://xwmtp.github.io/bingo2021/"}
         image={
-          "https://raw.githubusercontent.com/xwmtp/bingo2022/assets/images/previousYears/2021/background_480p.png"
+          "https://raw.githubusercontent.com/xwmtp/bingo-tournament-website-2023/assets/images/previousYears/2021/background_480p.png"
         }
       />
       <ImageBlock
         text={"2020"}
         url={"https://xwmtp.github.io/bingo2020/"}
         image={
-          "https://raw.githubusercontent.com/xwmtp/bingo2022/assets/images/previousYears/2020/background_480p.png"
+          "https://raw.githubusercontent.com/xwmtp/bingo-tournament-website-2023/assets/images/previousYears/2020/background_480p.png"
         }
       />
-    </>
+    </Container>
   );
 };
 
-const ImageBlockBottomMargin = styled(ImageBlock)`
-  margin-bottom: 1.2rem;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
 `;
